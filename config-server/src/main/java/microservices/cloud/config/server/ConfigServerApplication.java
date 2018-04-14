d6d4e33eacff928004abc8d0942b6a9670979039
Tomokazu Tantaka <tantaka.tomokazu@gmai.com>
2018-04-14 12:07:43 +0900

    aa

--- config-server/src/main/java/microservices/cloud/config/server/ConfigServerApplication.java
+++ config-server/src/main/java/microservices/cloud/config/server/ConfigServerApplication.java
@@ -1,11 +1,14 @@
 package microservices.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableDiscoveryClient
+
 @EnableConfigServer
+@SpringBootApplication
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
