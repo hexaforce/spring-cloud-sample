d25b612267eb33d9b2c27fada476347ae60d6fbd
Tomokazu Tantaka <tantaka.tomokazu@gmai.com>
2018-04-25 23:42:04 +0900

    aa

--- configuration-server/src/main/java/microservices/cloud/configuration/server/ConfigurationServerApplication.java
+++ configuration-server/src/main/java/microservices/cloud/configuration/server/ConfigurationServerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import microservices.cloud.service.utile.ServiceUtileApplication;
 import microservices.cloud.service.utile.StartupCompleteEvent;
 
 @EnableDiscoveryClient
@@ -20,6 +21,7 @@ public class ConfigurationServerApplication {
 	public static void main(String[] args) throws BeansException, IOException {
 		
 		ConfigurableApplicationContext context = new SpringApplicationBuilder()
+				.sources(ServiceUtileApplication.class)
 				.sources(ConfigurationServerApplication.class)
 				.listeners(new ApplicationPidFileWriter())
 				.run(args);
